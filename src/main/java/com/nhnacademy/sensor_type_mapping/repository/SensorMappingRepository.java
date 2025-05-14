de514d99118fa9760708a6610ab3f958c2919f93
nink2458 <nink2458@gmail.com>
2025-05-14 14:38:48 +0900

    추가 사항: 프로젝트 구성 추가

--- src/main/java/com/nhnacademy/sensor_type_mapping/repository/SensorMappingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.sensor_type_mapping.repository;
-
-import com.nhnacademy.sensor_type_mapping.domain.SensorMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensorMappingRepository extends JpaRepository<SensorMapping, Long>, CustomSensorMappingRepository {
-
-}
