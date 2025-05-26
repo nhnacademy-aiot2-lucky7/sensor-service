633b2c94ef7ae086835c449770dfc331833a548f
nink2458 <nink2458@gmail.com>
2025-05-26 10:24:43 +0900

    변경 사항: `DataTypeInfo` Validated DataType Size 통일화

--- src/main/java/com/nhnacademy/type/domain/DataTypeInfo.java
+++ src/main/java/com/nhnacademy/type/domain/DataTypeInfo.java
@@ -9,11 +9,11 @@ import lombok.Value;
 public class DataTypeInfo {
 
     @NotBlank(message = "데이터 타입의 영문명이 누락되었습니다.")
-    @Size(min = 2, max = 30, message = "데이터 타입의 영문명은 2자 이상 30자 이하로 입력해야 합니다.")
+    @Size(min = 2, max = 50, message = "데이터 타입의 영문명은 2자 이상 50자 이하로 입력해야 합니다.")
     @JsonProperty("type_en_name")
     String dataTypeEnName;
 
-    @Size(min = 2, max = 30, message = "데이터 타입의 한글명은 2자 이상 30자 이하로 입력해야 합니다.")
+    @Size(min = 2, max = 50, message = "데이터 타입의 한글명은 2자 이상 50자 이하로 입력해야 합니다.")
     @JsonProperty("type_kr_name")
     String dataTypeKrName;
 }
