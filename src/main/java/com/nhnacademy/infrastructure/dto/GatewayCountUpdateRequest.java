6b64cdca42b6c2c0d0003847b1a06173f81783c5
nink2458 <nink2458@gmail.com>
2025-06-06 17:53:49 +0900

    변경 사항: `GatewayCountUpdateRequest` DTO에 불필요한 valid annotation을 제거

--- src/main/java/com/nhnacademy/infrastructure/dto/GatewayCountUpdateRequest.java
+++ src/main/java/com/nhnacademy/infrastructure/dto/GatewayCountUpdateRequest.java
@@ -3,7 +3,6 @@ package com.nhnacademy.infrastructure.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 
 @Getter
@@ -12,8 +11,6 @@ public final class GatewayCountUpdateRequest {
     @NotNull
     private final Long gatewayId;
 
-    @NotNull
-    @PositiveOrZero
     private final Integer sensorCount;
 
     @JsonCreator
